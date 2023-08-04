2042374dfeafa8d765a0793baa5bd798f0c1c4d7
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-04 20:29:07 +0200

    JavaScriptExercises: 04-removeFromArray done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr) {
+  const args = Array.from(arguments).slice(1);
+  return arr.filter((curr) => !args.includes(curr));
 };
 
 // Do not edit below this line
