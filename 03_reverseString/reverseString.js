0571815cc71e4d2cec4fa8035fa04181b17ce1fb
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-04 19:58:42 +0200

    JavaScriptExercises: 03-reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (src) {
+  let toRet = "";
+  for (let i = src.length - 1; i >= 0; i--) {
+    toRet += src[i];
+  }
+  return toRet;
 };
 
 // Do not edit below this line
