cd137e03f9b51af4fee8da1df3cdf49ac72baba1
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-04 20:00:42 +0200

    JavaScriptExercises: 03-reverseString better solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
 const reverseString = function (src) {
-  let toRet = "";
-  for (let i = src.length - 1; i >= 0; i--) {
-    toRet += src[i];
-  }
-  return toRet;
+  return src.split("").reverse.join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
