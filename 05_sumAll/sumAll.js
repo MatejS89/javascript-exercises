46731bcaeb6c119906062edd5d9a311de67a3b79
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-04 20:44:11 +0200

    JavaScriptExercises: 05sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  let smaller = 0;
+  let larger = 0;
+  num1 <= num2
+    ? ((smaller = num1), (larger = num2))
+    : ((smaller = num2), (larger = num1));
+  let toRet = 0;
+  for (let i = smaller; i <= larger; i++) toRet += i;
+  return toRet;
 };
 
 // Do not edit below this line
