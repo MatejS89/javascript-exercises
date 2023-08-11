3efedc66cffac76ead58e328aad2808d464edc49
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-11 16:14:39 +0200

    JavaScriptExercises: 12 findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.sort((left, right) =>
+    left.yearOfDeath - left.yearOfBirth > right.yearOfDeath - right.yearOfBirth
+      ? -1
+      : 1
+  )[0];
 };
 
 // Do not edit below this line
