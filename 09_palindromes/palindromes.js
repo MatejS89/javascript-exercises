3a487e3c7ff438080f9f751f3f861eb5a0575739
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-11 15:49:18 +0200

    JavaScriptExercises: 09 Palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let processed = word.toLowerCase().replace(/[^0-9a-z]/g, "");
+  return processed.split("").reverse().join("") === processed;
 };
 
 // Do not edit below this line
