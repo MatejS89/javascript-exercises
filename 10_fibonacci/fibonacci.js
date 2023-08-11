b85243242051abfdf89e61450528d5c49ffaf529
Matej Sedlák <111086657+MatejS89@users.noreply.github.com>
2023-08-11 15:57:54 +0200

    JavaScriptExercises: 10 Fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (targetNum) {
+  if (targetNum <= 0) return "OOPS";
+  const targetIndex = targetNum - 1;
+  let sequence = [1, 1];
+  for (let i = 1; i < targetIndex; i++) {
+    sequence[i + 1] = sequence[i] + sequence[i - 1];
+  }
+  return sequence[targetIndex];
 };
 
 // Do not edit below this line
